6a1239bc1eb826b4e1f1e1ac36c6446d555eac20
vikaputri <vikaputri909@gmail.com>
2020-12-03 07:24:20 +0700

    second update

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'App',
+    'register'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_REDIRECT_URL = "../"
+LOGOUT_REDIRECT_URL = "/"
